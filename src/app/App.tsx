21314696d3985c7f3079059f3b4ab4c25c14537b
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2024-06-02 23:51:55 +0300

    feat: добавить проверку авторизации при загрузке страницы

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,39 @@
+import { Router } from '@app/Router/Router.tsx';
+import { useRefreshTokensQuery } from '@features/auth/auth-api.slice.ts';
+import { ISetCredentialsPayload } from '@features/auth/auth.interface.ts';
+import { setCredentials } from '@features/auth/auth.slice.ts';
+import { useTypedSelector } from '@shared/libs/hooks/useTypedSelector.ts';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 const App = () => {
-  return <></>;
+  const [isAuth, setIsAuth] = useState<string>('loading');
+  const { user } = useTypedSelector(state => state.auth);
+  const { data, isLoading } = useRefreshTokensQuery<{
+    data: ISetCredentialsPayload;
+    isLoading: boolean;
+  }>(null);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (data) {
+      dispatch(setCredentials(data));
+    }
+    if (isLoading === false && !data) setIsAuth('not-auth');
+  }, [isLoading]);
+
+  useEffect(() => {
+    if (user) {
+      setIsAuth('auth');
+    }
+  }, [user]);
+
+  return (
+    <BrowserRouter>
+      <Router isAuth={isAuth} />
+    </BrowserRouter>
+  );
 };
-export default App;
+export { App };
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { store } from '@app/store/store.ts';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './app/App.tsx';
+import { App } from './app/App.tsx';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
