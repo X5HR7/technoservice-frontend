1a6b4920871ad772f5d2da74b5bd2b7daf90cbdd
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2024-06-03 09:50:46 +0300

    feat: реализовать страницу 403

--- src/pages/Page403/ui/Page403.tsx
+++ src/pages/Page403/ui/Page403.tsx
@@ -1,10 +1,20 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Page403.module.scss';
 
 const Page403: FC = () => {
   return (
-    <div>
-      403 Page
-    </div>
+    <main className={styles.main}>
+      <section className={styles.section}>
+        <div className={styles['error-page__text-content']}>
+          <h1 className={styles['error-page__title']}>403</h1>
+          <p className={styles['error-page__subtitle']}>Нет прав доступа</p>
+        </div>
+        <Link to='/' className={styles.link}>
+          Назад
+        </Link>
+      </section>
+    </main>
   );
 };
 
