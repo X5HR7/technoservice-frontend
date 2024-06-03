01fb6b7fd05911c23725988a597a8d963e897fc2
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2024-06-03 09:48:19 +0300

    feat: реализовать страницу 404

--- src/pages/Page404/ui/Page404.tsx
+++ src/pages/Page404/ui/Page404.tsx
@@ -1,10 +1,18 @@
 import React, { FC } from 'react';
+import styles from './Page404.module.scss';
+import { Link } from 'react-router-dom';
 
 const Page404: FC = () => {
   return (
-    <div>
-      404
-    </div>
+    <main className={styles.main}>
+      <section className={styles.section}>
+        <div className={styles['error-page__text-content']}>
+          <h1 className={styles['error-page__title']}>404</h1>
+          <p className={styles['error-page__subtitle']}>Страница не найдена</p>
+        </div>
+        <Link to='/' className={styles.link}>Назад</Link>
+      </section>
+    </main>
   );
 };
 
