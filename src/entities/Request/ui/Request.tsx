fedf0d691d4fc1d845467b61e2aedefdac40ef6e
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2024-06-05 11:02:13 +0300

    feat: создать реализовать сущность для отображения запроса

--- src/entities/Request/ui/Request.tsx
+++ src/entities/Request/ui/Request.tsx
@@ -1,10 +1,40 @@
-import React, { FC } from 'react';
-import styles from './Request.module.scss'
 import { IRequestProps } from '@entities/Request/model/request.interface.ts';
+import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Request.module.scss';
 
-const Request: FC<IRequestProps> = () => {
+const Request: FC<IRequestProps> = ({ request }) => {
+  const { id, number, equipment, type, description, status, createdAt } = request;
+
   return (
-    <li className={styles.requests__item}></li>
+    <li className={styles.requests__item}>
+      <Link to={`/requests/${id}`} replace={true} className={styles.link}>
+        <div className={styles.request}>
+          <div className={styles.request__col}>
+            <p className={styles['request__col-title']}>Номер заявки</p>
+            <p className={styles['request__col-value']}>{number}</p>
+          </div>
+          <div className={styles.request__col}>
+            <p className={styles['request__col-title']}>Оборудование</p>
+            <p className={styles['request__col-value']}>{equipment}</p>
+          </div>
+          <div className={styles.request__col}>
+            <p className={styles['request__col-title']}>Тип неисправности</p>
+            <p className={styles['request__col-value']}>{type}</p>
+          </div>
+          <div className={styles.request__col}>
+            <p className={styles['request__col-title']}>Статус</p>
+            <p className={styles['request__col-value']}>{status}</p>
+          </div>
+          <div className={styles.request__col}>
+            <p className={styles['request__col-title']}>Создан</p>
+            <p className={styles['request__col-value']}>
+              {new Date(createdAt).toLocaleDateString()}
+            </p>
+          </div>
+        </div>
+      </Link>
+    </li>
   );
 };
 
