4be85aff54a2c0d490ab1b06a6bd46404419a749
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2024-06-05 09:29:16 +0300

    fix: добавить пропс onClick в компонент Button.tsx

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,19 +1,30 @@
 import React, { FC } from 'react';
-import styles from './Button.module.scss'
+import styles from './Button.module.scss';
 
 interface IButton {
   disabled: boolean;
   appearance: 'positive' | 'negative' | 'neutral';
   mode: 'primary' | 'secondary';
+  onClick: () => void;
   children?: any;
 }
 
-const Button: FC<IButton> = ({appearance='positive', disabled=false, mode='primary', children}) => {
+const Button: FC<IButton> = ({
+  appearance = 'positive',
+  disabled = false,
+  mode = 'primary',
+  children,
+  onClick
+}) => {
   return (
-    <button className={`${styles.button} ${styles[appearance]} ${styles[mode]}`} disabled={disabled}>
+    <button
+      className={`${styles.button} ${styles[appearance]} ${styles[mode]}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
 };
 
-export {Button};
+export { Button };
