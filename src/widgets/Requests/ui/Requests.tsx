2b5d7aa9bf925e52d51e039012a19c3d82d82b6d
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2024-06-05 11:04:04 +0300

    feat: реализовать виджет для отображения заявок пользователя

--- src/widgets/Requests/ui/Requests.tsx
+++ src/widgets/Requests/ui/Requests.tsx
@@ -1,40 +1,64 @@
-import { IRequests } from '@widgets/Requests/model/requests.interface.ts';
-import React, { FC } from 'react';
-import styles from './Requests.module.scss';
 import { Request } from '@entities/Request';
+import { CreateRequestForm } from '@features/CreateRequestForm';
+import { IRequest } from '@shared/libs/interfaces';
+import { Button } from '@shared/ui';
+import { Modal } from '@widgets/Modal';
+import { IRequests } from '@widgets/Requests/model/requests.interface.ts';
+import React, { FC, useState } from 'react';
+import styles from './Requests.module.scss';
 
-const Requests: FC<IRequests> = ({ requests }) => {
+const Requests: FC<IRequests> = ({ requests: requestsArr }) => {
+  const [isOpened, setIsOpened] = useState(false);
+  const [requests, setRequests] = useState<IRequest[]>(requestsArr);
+
   const currentRequests = requests?.filter(request => !Boolean(request.completedAt));
   const completedRequests = requests?.filter(requests => Boolean(requests.completedAt));
 
+  const handleOpenModalButtonClick = () => {
+    setIsOpened(true);
+  };
+
+  const closeModal = () => {
+    setIsOpened(false);
+  };
+
   return (
     <>
+      <Modal isOpened={isOpened} setIsOpened={setIsOpened} className={styles.modal}>
+        <CreateRequestForm
+          isModalOpened={isOpened}
+          closeModal={closeModal}
+          setRequests={setRequests}
+        />
+      </Modal>
       <section className={styles['request-section']}>
-        <h2 className={styles['request-section__header']}>Текущие заявки</h2>
+        <div className={styles.header}>
+          <h2 className={styles['request-section__header']}>Текущие заявки</h2>
+          <Button
+            disabled={false}
+            appearance={'positive'}
+            mode={'secondary'}
+            onClick={handleOpenModalButtonClick}
+          >
+            Новая заявка
+          </Button>
+        </div>
         <ul className={styles.requests}>
-          {
-            currentRequests.length > 0 ? (
-              currentRequests.map(request => (
-               <Request request={request} key={request.id} />
-              ))
-            ) : (
-              <p className={styles.request__not_found}>Обрабатываемых заявок на данный момент нет!</p>
-            )
-          }
+          {currentRequests.length > 0 ? (
+            currentRequests.map(request => <Request request={request} key={request.id} />)
+          ) : (
+            <p className={styles.request__not_found}>Обрабатываемых заявок на данный момент нет!</p>
+          )}
         </ul>
       </section>
       <section className={styles['request-section']}>
         <h2 className={styles['request-section__header']}>История заявок</h2>
         <ul className={styles.requests}>
-          {
-            completedRequests.length > 0 ? (
-              completedRequests.map(request => (
-                <Request request={request} key={request.id} />
-              ))
-            ) : (
-              <p className={styles.request__not_found}>Вы никогда не оставляли у нас заявки!</p>
-            )
-          }
+          {completedRequests.length > 0 ? (
+            completedRequests.map(request => <Request request={request} key={request.id} />)
+          ) : (
+            <p className={styles.request__not_found}>Вы никогда не оставляли у нас заявки!</p>
+          )}
         </ul>
       </section>
     </>
