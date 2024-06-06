bf55ca676c7d45bf00258adef7dbd9d0bdcfe920
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2024-06-06 12:26:07 +0300

    feat: создать виждет чата

---
+++ src/widgets/Chat/ui/Chat.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import { IChat } from '@widgets/Chat/model/chat.interface.ts';
+import styles from './Chat.module.scss';
+
+const Chat: FC<IChat> = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export {Chat};
