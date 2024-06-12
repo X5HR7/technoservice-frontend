0ad349fd0c22a24139752a565fe985fc3d77ff6a
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2024-06-13 01:54:44 +0300

    feat: добавить поле client

--- src/shared/libs/interfaces/full-request.interface.ts
+++ src/shared/libs/interfaces/full-request.interface.ts
@@ -1,10 +1,11 @@
+import { IComment } from '@shared/libs/interfaces/comment.interface.ts';
+import { INotification } from '@shared/libs/interfaces/notification.interface.ts';
 import { IRequest } from '@shared/libs/interfaces/request.interface.ts';
 import { IUser } from '@shared/libs/interfaces/user.interface.ts';
-import { INotification } from '@shared/libs/interfaces/notification.interface.ts';
-import { IComment } from '@shared/libs/interfaces/comment.interface.ts';
 
 export interface IFullRequest extends IRequest {
+  client?: IUser;
   master: IUser;
   notifications: INotification[];
-  comments: IComment[]
+  comments: IComment[];
 }
