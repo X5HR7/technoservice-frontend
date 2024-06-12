462050d0fea678525f1ad56c244b72a25f54b828
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2024-06-13 01:54:07 +0300

    feat: реализовать страницу мастера

--- src/pages/MasterRequestsPage/ui/MasterRequestsPage.tsx
+++ src/pages/MasterRequestsPage/ui/MasterRequestsPage.tsx
@@ -1,10 +1,34 @@
-import React, { FC } from 'react';
+import { useGetMasterRequestsQuery } from '@features/request/request-api.slice.ts';
+import { useTypedSelector } from '@shared/libs/hooks/useTypedSelector.ts';
+import { Spinner } from '@shared/ui';
+import { Header } from '@widgets/Header';
+import { Requests } from '@widgets/Requests';
+import React, { FC, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './MasterRequestsPage.module.scss';
 
 const MasterRequestsPage: FC = () => {
+  const navigate = useNavigate();
+  const currentUser = useTypedSelector(state => state.auth.user);
+
+  const { data: requests, isLoading } = useGetMasterRequestsQuery(null);
+
+  useEffect(() => {
+    if (currentUser?.role !== 'master') navigate('/403', { replace: true });
+  }, []);
+
   return (
-    <div>
-      Master Requests Page
+    <div className={styles.page}>
+      <Header />
+      <main className={styles.main}>
+        {isLoading ? (
+          <Spinner size={'large'} />
+        ) : (
+          <section className={styles.requests}>
+            <Requests requests={requests} />
+          </section>
+        )}
+      </main>
     </div>
   );
 };
