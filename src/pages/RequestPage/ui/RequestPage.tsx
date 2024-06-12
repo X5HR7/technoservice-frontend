9b34e86a22b79d6708a6c7556de42cdc80f37492
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2024-06-13 01:55:20 +0300

    refactor: вынести список запросов в состояние

--- src/pages/RequestPage/ui/RequestPage.tsx
+++ src/pages/RequestPage/ui/RequestPage.tsx
@@ -1,26 +1,26 @@
 import { useGetUserRequestQuery } from '@features/request/request-api.slice.ts';
-import { useTypedSelector } from '@shared/libs/hooks/useTypedSelector.ts';
 import { IFullRequest } from '@shared/libs/interfaces';
 import { Spinner } from '@shared/ui';
 import { Chat } from '@widgets/Chat';
 import { Header } from '@widgets/Header';
 import { RequestInfo } from '@widgets/RequestInfo';
 import { RequestNotificationHistory } from '@widgets/RequestNotificationHistory';
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './RequestPage.module.scss';
 
 const RequestPage: FC = () => {
-  const currentUser = useTypedSelector(state => state.auth.user);
   const { id: requestId } = useParams();
-  const { data: request, isLoading } = useGetUserRequestQuery<{
+  const { data: requestResponse, isLoading } = useGetUserRequestQuery<{
     data: IFullRequest;
     isLoading: boolean;
   }>(requestId as string);
 
+  const [request, setRequest] = useState<IFullRequest>();
+
   useEffect(() => {
     if (!isLoading) {
-      console.log(request);
+      setRequest(requestResponse);
     }
   }, [isLoading]);
 
@@ -28,18 +28,18 @@ const RequestPage: FC = () => {
     <div className={styles.page}>
       <Header />
       <main className={styles.main}>
-        {isLoading ? (
+        {isLoading || !request ? (
           <Spinner size={'large'} />
         ) : (
           <>
             <section className={styles.info}>
-              <RequestInfo request={request} />
+              <RequestInfo request={request as IFullRequest} setRequest={setRequest} />
             </section>
             <section className={styles.chat}>
-              <Chat messages={request.comments} requestId={request.id} />
+              <Chat messages={(request as IFullRequest)?.comments} requestId={(request as IFullRequest)?.id} />
             </section>
             <section className={styles.history}>
-              <RequestNotificationHistory notifications={request.notifications} />
+              <RequestNotificationHistory notifications={(request as IFullRequest)?.notifications} />
             </section>
           </>
         )}
