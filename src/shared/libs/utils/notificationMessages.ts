f3faeaf1665ba29a0dd8ccff3ec1a78f8fa46403
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2024-06-13 01:46:49 +0300

    refactor: использовать кастомный текст для описания уведомлений (1)

---
+++ src/shared/libs/utils/notificationMessages.ts
@@ -0,0 +1,5 @@
+export const notificationMessages = {
+  'request-created': 'Заявка создана и находится в обработке',
+  'request-master-set': 'На выполнение заявки назначен мастер',
+  'request-status-updated': 'Статус заявки обновлен'
+}
