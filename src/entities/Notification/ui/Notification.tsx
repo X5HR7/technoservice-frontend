e21164c9eb6aa0ec849c7c83cb10bf5ffc58c014
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2024-06-13 01:46:31 +0300

    refactor: использовать кастомный текст для описания уведомлений

--- src/entities/Notification/ui/Notification.tsx
+++ src/entities/Notification/ui/Notification.tsx
@@ -1,4 +1,5 @@
 import { INotification } from '@shared/libs/interfaces';
+import { notificationMessages } from '@shared/libs/utils/notificationMessages.ts';
 import React, { FC } from 'react';
 import styles from './Notification.module.scss';
 
@@ -6,7 +7,13 @@ const Notification: FC<INotification> = ({ event }) => {
   return (
     <li className={styles.notification}>
       <p className={styles.notification__title}>Событие:</p>
-      <p className={styles.notification__text}>{event}</p>
+      <p className={styles.notification__text}>
+        {
+          notificationMessages[
+            event as 'request-created' | 'request-master-set' | 'request-status-updated'
+          ]
+        }
+      </p>
     </li>
   );
 };
